355d2f0fffd9509e2b4b8d1dbcc5e6355b69f31c
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2017-11-30 20:10:52 +0530

    db initial commit

---
+++ playground/mongo-connect.js
@@ -0,0 +1,43 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if(err){
+		return console.log("Unable to connect the Mongo server")
+	};
+	console.log("Server connected");
+
+	// db.collection('TodoApp').insertOne({
+	// 	text: 'vikas tiwari',
+	// 	completed: false
+	// }, (err, result) => {
+	// 	if(err){
+	// 		console.log("unable to eInsert data", err);
+	// 	};
+	// 	console.log(JSON.stringify(result.ops, undefined, 2))
+	// })
+
+	// db.collection('Users').insertOne({
+	// 	name: 'vikas tiwari',
+	// 	age: 20, 
+	// 	location: "Jabalpur",
+	// 	completed: false,
+	// 	_id:1234
+	// }, (err, result) => {
+
+	// 	if(err){
+	// 		console.log("unable to eInsert data", err);
+	// 	};
+	// 	console.log(JSON.stringify(result.ops, undefined, 2))
+	// })
+
+	db.collection('Users').find().toArray().then((result)=>{
+		console.log(result)
+	}, (err)=> {
+		console.log('Unable to fetch the data', err)
+	})
+
+	// db.close()
+})
+
+// C:\Program Files\MongoDB\Server\3.4\bin mongo.exe --dbpath C:\users\chandu\mongo-data
