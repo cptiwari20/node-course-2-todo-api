6c344a0b450633e7695828e2bff2ad0e3a984db5
cptiwari20 <cptiwari20@gmail.com>
2018-05-27 16:36:44 +0530

    Adding to Heroku Again

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||
+mongoose.connect( 'mongodb://node-user:pass1234@ds239309.mlab.com:39309/node-todo-api' ||
 	'mongodb://localhost:27017/new-Todo', {useMongoClient: true});
 
 module.exports = {
