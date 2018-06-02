14498e000848e3a6dfc53c9c0f9e8cfd9753d631
cptiwari20 <cptiwari20@gmail.com>
2018-06-02 18:45:22 +0530

    testing the logout route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -287,4 +287,25 @@ describe('POST /user/login', () =>{
       })
     })
   })
+});
+
+// logout route
+describe('DELETE /user/me/token', () =>{
+  it('should logout the user', (done) =>{
+    request(app)
+      .delete('/user/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) =>{
+        if(err){
+          done(err)
+        } 
+        User.findById(users[0]._id).then((user)=>{
+          expect(user.tokens.length).toBe(0)
+          done()
+        }).catch((e) =>{
+          done(e)
+        })
+      })
+  })
 })
