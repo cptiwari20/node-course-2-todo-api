651aa507585a5b542a72d08cf020731c574a1e3d
cptiwari20 <cptiwari20@gmail.com>
2018-06-02 17:35:54 +0530

    adding login route

--- server/server.js
+++ server/server.js
@@ -98,11 +98,23 @@ app.post("/users", (req, res) => {
 	})
 });
 
-
 app.get('/users/me', authenticate, (req, res)=> {
 	res.send(req.user);
-})
+});
 
+//login routes
+app.post('/user/login', (req, res) =>{
+	var body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password).then((user) =>{
+		// return user.generateAuthToken().then((token)=>{
+		// 	res.header('x-auth', token).send(user);		
+		// })
+		res.send(user)
+	}).catch((e) =>{
+		res.status(400).send(e);
+	})
+})
 app.listen(port, () => {
 	console.log("local server has been started! its port:", port)
 });
